fde506db12bd1a24b8660b06d74c12a5dea2ee0f
HirushaR <hirusharandunu11@gmail.com>
2020-07-22 20:56:04 +0530

    digit reader

--- lex/Program.cs
+++ lex/Program.cs
@@ -23,4 +23,72 @@ namespace lex
             }
         }
     }
+
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+        }
+
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text { get; }
+    }
+
+
+    class Lexer
+    {
+        private readonly string _text;
+        private int _position;
+
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+        private char Current
+        {
+            get
+            {
+                if(_position >= _text.Length)
+                {
+                    return '\0';
+                }
+                return _text[_position];
+            }
+        }
+
+        private void Next()
+        {
+            _position++;
+        }
+
+        // every time we called next token we check next item and keep going
+        public SyntaxToken NextToken()
+        {
+            //<numbers>
+            //+-*?()
+            //<whitespace>
+
+            if(char.IsDigit(Current))
+            {
+                var start = _position;
+
+                while (char.IsDigit(Current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text);
+            }
+            
+        }
+    }
+
+    enum SyntaxKind
+    {
+        NumberToken
+    }
 }
