345b424c839ad32192d27a1c47f36aac51f9469c
HirushaR <hirusharandunu11@gmail.com>
2020-07-23 21:30:49 +0530

    Unary Operator evaluate

--- lex/CodeAnalysis/Evaluator.cs
+++ lex/CodeAnalysis/Evaluator.cs
@@ -27,6 +27,18 @@ namespace lex.CodeAnalysis
             if (node is LiteralExpressionSyntax n)
                 return (int)n.NumberToken.Value;
 
+            if( node is UnaryExpressionSyntax u)
+            {
+                var operand = EvaluateExpression(u.Operand);
+
+                if (u.OperatorToken.Kind == SyntaxKind.PlusToken)
+                    return operand;
+                else if (u.OperatorToken.Kind == SyntaxKind.MinusToken)
+                    return -operand;
+                else
+                    throw new Exception($"Unexpected Unary operator {u.OperatorToken.Kind}");
+            }
+
             if (node is BinaryExpressionSyntax b)
             {
                 var left = EvaluateExpression(b.Left);
