7e76c3a74d2f45f68da7674d6ba6a0816525c220
HirushaR <hirusharandunu11@gmail.com>
2020-07-28 23:01:18 +0530

    add veriable symble class

---
+++ Lex/CodeAnalysis/VariableSymble.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lex.CodeAnalysis
+{
+    public sealed class VariableSymble
+    {
+        public VariableSymble(string name, Type type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; }
+        public Type Type { get; }
+    }
+
+}
