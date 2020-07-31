87864e970a72838b1e38070bb50b1e617188fd35
HirushaR <hirusharandunu11@gmail.com>
2020-07-31 15:37:57 +0530

    Parser_BinaryExpression_honorsPrecedences

--- Lex.Tests/CodeAnalysis/Syntax/ParserTests.cs
+++ Lex.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -10,12 +10,37 @@ namespace Lex.Tests.CodeAnalysis.Syntax
     public class ParserTests
     {
         [Theory]
-        [MemberData(GetBinaryOperatorPairsData)]
+        [MemberData(nameof(GetBinaryOperatorPairsData))]
         public void Parser_BinaryExpression_honorsPrecedences(SyntaxKind op1, SyntaxKind op2)
         {
+            var op1Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op1);
+            var op2Precedence = SyntaxFacts.GetBinaryOperatorPrecedence(op2);
+            var op1Text = SyntaxFacts.GetText(op1);
+            var op2Text = SyntaxFacts.GetText(op2);
+            var text = $"a {op1Text} b {op2Text} c";
 
+            if (op1Precedence >= op2Precedence)
+            {
+                Assert.False(true);
+            }
+            else
+            {
+                Assert.False(true);
+            }
         }
 
-     
+        public static IEnumerable<object[]> GetBinaryOperatorPairsData()
+        {
+            foreach(var op1 in SyntaxFacts.GetBinaryOperatorsKinds())
+            {
+                foreach (var op2 in SyntaxFacts.GetBinaryOperatorsKinds())
+                {
+                    yield return new object[] { op1, op2 };
+                }
+            }
+        }
+
+
+
     }
 }
