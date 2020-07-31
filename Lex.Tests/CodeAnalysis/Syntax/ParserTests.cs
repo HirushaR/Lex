634678e39550dee839d1d19569f3a34e6a97a591
HirushaR <hirusharandunu11@gmail.com>
2020-07-31 15:45:31 +0530

    AssertingEnumerator class with  DIspolse interface

--- Lex.Tests/CodeAnalysis/Syntax/ParserTests.cs
+++ Lex.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -1,10 +1,50 @@
 using Lex.CodeAnalysis.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Lex.Tests.CodeAnalysis.Syntax
 {
+
+    internal sealed class AssertingEnumerator: IDisposable
+    {
+
+        private IEnumerator<SyntaxNode> _enumerator;
+        public AssertingEnumerator(SyntaxNode node)
+        {
+            _enumerator = Flatten(node).GetEnumerator();
+        }
+        public void Dispose()
+        {
+            Assert.False(_enumerator.MoveNext());
+            _enumerator.Dispose();
+        }
+
+        private static IEnumerable<SyntaxNode> Flatten(SyntaxNode node)
+        {
+            var stack = new Stack<SyntaxNode>();
+            stack.Push(node);
+
+            while(stack.Count > 0)
+            {
+                var n = stack.Pop();
+                yield return n;
+
+                foreach (var child in n.GetChildren().Reverse())
+                    stack.Push(child);
+            }
+        }
+        public void AssertToken(SyntaxKind kind, string text)
+        {
+            Assert.True(_enumerator.MoveNext());
+            var token = Assert.IsType<SyntaxToken>(_enumerator.Current);
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text)
+        }
+
+       
+    }
 
 
     public class ParserTests
