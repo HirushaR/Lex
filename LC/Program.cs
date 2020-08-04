47d3c152e00a6f4ca75a088be6c05ce317e277ec
HirushaR <hirusharandunu11@gmail.com>
2020-08-04 22:51:09 +0530

    Move prettyPrint to SyntaxNode

--- LC/Program.cs
+++ LC/Program.cs
@@ -44,7 +44,8 @@ namespace Lex
                 if (showTree)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkGray;
-                    PrettyPrint(syntaxTree.Root);
+                    syntaxTree.Root.WriteTo(Console.Out);
+                   // PrettyPrint(syntaxTree.Root);
                     Console.ResetColor();
                 }
 
@@ -78,38 +79,11 @@ namespace Lex
                         Console.Write(suffix);
 
                         Console.WriteLine();
-                    }
-
-
-                     
-
-                    
+                    }                    
                 }
             }
         }
 
-        static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
-        {
-            var marker = isLast ? "└──" : "├──";
-
-            Console.Write(indent);
-            Console.Write(marker);
-            Console.Write(node.Kind);
-
-            if (node is SyntaxToken t && t.Value != null)
-            {
-                Console.Write(" ");
-                Console.Write(t.Value);
-            }
-
-            Console.WriteLine();
-
-            indent += isLast ? "   " : "│   ";
-
-            var lastChild = node.GetChildren().LastOrDefault();
-
-            foreach (var child in node.GetChildren())
-                PrettyPrint(child, indent, child == lastChild);
-        }
+      
     }
 }
--- Lex/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Lex/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -34,6 +35,43 @@ namespace Lex.CodeAnalysis.Syntax
                     foreach(var child in childern)
                         yield return child;
                 }
+            }
+        }
+
+        public void WriteTo(TextWriter writer)
+        {
+            PrettyPrint(writer,this);
+        }
+        private static void PrettyPrint(TextWriter writer, SyntaxNode node, string indent = "", bool isLast = true)
+        {
+            var marker = isLast ? "└──" : "├──";
+
+            writer.Write(indent);
+            writer.Write(marker);
+            writer.Write(node.Kind);
+
+            if (node is SyntaxToken t && t.Value != null)
+            {
+                writer.Write(" ");
+                writer.Write(t.Value);
+            }
+
+            writer.WriteLine();
+
+            indent += isLast ? "   " : "│   ";
+
+            var lastChild = node.GetChildren().LastOrDefault();
+
+            foreach (var child in node.GetChildren())
+                PrettyPrint(writer, child, indent, child == lastChild);
+        }
+
+        public override string ToString() 
+        {
+            using (var writer = new StringWriter())
+            {
+                WriteTo(writer);
+                return writer.ToString();
             }
         }
     }
