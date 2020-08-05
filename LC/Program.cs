a3569eb497c597c4e87bad7109406721ec5dd96f
HirushaR <hirusharandunu11@gmail.com>
2020-08-05 22:10:37 +0530

    Add colorisation

--- LC/Program.cs
+++ LC/Program.cs
@@ -19,10 +19,13 @@ namespace Lex
 
             while (true)
             {
+                Console.ForegroundColor =ConsoleColor.Green;
                 if( textBuilder.Length == 0)
-                    Console.Write("> ");
+                    Console.Write("» ");
                 else
-                    Console.Write("|");
+                    Console.Write("·");
+                
+                Console.ResetColor();
                 
                 var input = Console.ReadLine();
                 var isBlank = string.IsNullOrWhiteSpace(input);
@@ -61,14 +64,15 @@ namespace Lex
                 if (showTree)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkGray;
-                    syntaxTree.Root.WriteTo(Console.Out);
-                   // PrettyPrint(syntaxTree.Root);
+                    syntaxTree.Root.WriteTo(Console.Out);               
                     Console.ResetColor();
                 }
 
                 if (!result.Diagnostics.Any())
                 {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine(result.Value);
+                    Console.ResetColor();
                 }
                 else
                 {                 
--- Lex/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Lex/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -45,17 +46,32 @@ namespace Lex.CodeAnalysis.Syntax
         }
         private static void PrettyPrint(TextWriter writer, SyntaxNode node, string indent = "", bool isLast = true)
         {
+            var isToConsole = writer == Console.Out;
+
             var marker = isLast ? "└──" : "├──";
 
             writer.Write(indent);
-            writer.Write(marker);
-            writer.Write(node.Kind);
 
+            if( isToConsole)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                writer.Write(marker);
+                Console.ResetColor();
+
+            }            
+            if( isToConsole)
+                Console.ForegroundColor =  node is SyntaxToken ? ConsoleColor.Blue : ConsoleColor.Cyan;  
+
+             writer.Write(node.Kind);
+        
             if (node is SyntaxToken t && t.Value != null)
             {
                 writer.Write(" ");
                 writer.Write(t.Value);
             }
+
+            if(isToConsole)
+                Console.ResetColor();
 
             writer.WriteLine();
 
