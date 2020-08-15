04c5713ffb14c37534fd020ddabefee7d36b4c53
HirushaR <hirusharandunu11@gmail.com>
2020-08-15 20:32:48 +0530

    swap-exepected-actual in xunit assertion

--- Lex.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ Lex.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -49,10 +49,10 @@ namespace Lex.Tests.CodeAnalysis.Syntax
             var tokens = SyntaxTree.ParseTokens(text).ToArray();
 
             Assert.Equal(2, tokens.Length);
-            Assert.Equal(tokens[0].Kind, t1Kind);
-            Assert.Equal(tokens[0].Text, t1Text);
-            Assert.Equal(tokens[1].Kind, t2Kind);
-            Assert.Equal(tokens[1].Text, t2Text);
+            Assert.Equal(t1Kind, tokens[0].Kind);
+            Assert.Equal(t1Text, tokens[0].Text);
+            Assert.Equal(t2Kind, tokens[1].Kind);
+            Assert.Equal(t2Text, tokens[1].Text);
         }
 
         [Theory]
@@ -65,12 +65,12 @@ namespace Lex.Tests.CodeAnalysis.Syntax
             var tokens = SyntaxTree.ParseTokens(text).ToArray();
 
             Assert.Equal(3, tokens.Length);
-            Assert.Equal(tokens[0].Kind, t1Kind);
-            Assert.Equal(tokens[0].Text, t1Text);
-            Assert.Equal(tokens[1].Kind, separatorKind);
-            Assert.Equal(tokens[1].Text, separatorText);
-            Assert.Equal(tokens[2].Kind, t2Kind);
-            Assert.Equal(tokens[2].Text, t2Text);
+            Assert.Equal(t1Kind, tokens[0].Kind);
+            Assert.Equal(t1Text, tokens[0].Text);
+            Assert.Equal(separatorKind, tokens[1].Kind);
+            Assert.Equal(separatorText, tokens[1].Text);
+            Assert.Equal(t2Kind, tokens[2].Kind);
+            Assert.Equal(t2Text,tokens[2].Text);
         }
 
         public static IEnumerable<object[]> GetTokensData()
