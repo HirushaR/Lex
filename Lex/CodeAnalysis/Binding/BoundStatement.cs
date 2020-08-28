c3a18b8df02a38d4adfea77e40656ddb00ff94b6
HirushaR <hirusharandunu11@gmail.com>
2020-08-28 10:45:12 +0530

    move to variable declaration

--- Lex/CodeAnalysis/Binding/BoundStatement.cs
+++ Lex/CodeAnalysis/Binding/BoundStatement.cs
@@ -4,18 +4,4 @@ namespace Lex.CodeAnalysis.Binding
     {
 
     }
-
-    internal sealed class BoundVeriableDeclaration : BoundStatement
-    {
-        public BoundVeriableDeclaration(VariableSymble variable, BoundExpression initializer)
-        {
-            Variable = variable;
-            Initializer = initializer;
-        }
-
-        public override BoundNodeKind Kind => BoundNodeKind.VariableDeclaration;
-        public VariableSymble Variable { get; }
-        public BoundExpression Initializer { get; }
-
-    }
 }
