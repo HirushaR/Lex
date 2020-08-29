febc0510cc7668e1a390aff529a0b6651c692ab5
HirushaR <hirusharandunu11@gmail.com>
2020-08-29 19:55:10 +0530

    add lable goto contitional goto satetmnt

--- Lex/CodeAnalysis/Binding/BoundConditionalGotoStatment.cs
+++ Lex/CodeAnalysis/Binding/BoundConditionalGotoStatment.cs
@@ -7,11 +7,14 @@ namespace Lex.CodeAnalysis.Binding
         public BoundConditionalGotoStatment(LabelSymbol label,BoundExpression condition,bool jumpIfFales =false)
         {
             Label = label;
-            
+            Condition = condition;
+            JumpIfFales = jumpIfFales;
         }
 
         public override BoundNodeKind Kind => BoundNodeKind.ConditionalGotoStatment;
 
         public LabelSymbol Label { get; }
+        public BoundExpression Condition { get; }
+        public bool JumpIfFales { get; }
     }
 }
