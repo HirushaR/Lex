1464b09dbbbfa594062ec1a77e5c09527199b733
HirushaR <hirusharandunu11@gmail.com>
2020-09-21 14:04:08 +0530

    add static type symbols int bool string

--- Lex/CodeAnalysis/Symbols/TypeSymbol.cs
+++ Lex/CodeAnalysis/Symbols/TypeSymbol.cs
@@ -1,7 +1,12 @@
 namespace Lex.CodeAnalysis.Symbols
 {
+
+   
     public sealed class TypeSymbol : Symbol
     {
+        public static readonly TypeSymbol Int = new TypeSymbol("int");
+        public static readonly TypeSymbol Bool = new TypeSymbol("bool");
+        public static readonly TypeSymbol String = new TypeSymbol("string");
         internal TypeSymbol(string name)
         : base(name)
         {
