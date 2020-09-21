f83fa0d0d64fd0029035dbd99d1bb21c7dea3ea8
HirushaR <hirusharandunu11@gmail.com>
2020-09-21 13:57:32 +0530

    create symbol calss

---
+++ Lex/CodeAnalysis/Symbols/Symbol.cs
@@ -0,0 +1,14 @@
+namespace Lex.CodeAnalysis.Symbols
+{
+    public abstract class Symbol
+    {
+        private protected Symbol(string name)
+        {
+            Name = name;
+        }
+
+        public abstract SymbolKind kind {get;}
+        public string Name { get; }
+    }
+
+}
