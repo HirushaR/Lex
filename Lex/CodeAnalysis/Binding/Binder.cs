3ba1d451a97c579a1713762acb7cc2ee18a15a2b
HirushaR <hirusharandunu11@gmail.com>
2020-10-05 18:31:13 +0530

    check syntax token is missing

--- Lex/CodeAnalysis/Binding/Binder.cs
+++ Lex/CodeAnalysis/Binding/Binder.cs
@@ -203,7 +203,7 @@ namespace Lex.CodeAnalysis.Binding
         private BoundExpression BindNameExpression(NameExpressionSyntax syntax)
         {
             var name = syntax.IdentifierToken.Text;
-            if(string.IsNullOrEmpty(name))
+            if(syntax.IdentifierToken.isMissing)
             {
                 // this means the token was inserted by the parser. We already
                 // reported the error we can just return an error expression
