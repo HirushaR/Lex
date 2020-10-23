c965ae3c4daffc6239a4f15d5e88fbd7efc158ee
HirushaR <hirusharandunu11@gmail.com>
2020-10-23 15:37:48 +0530

    check type to conver

--- Lex/CodeAnalysis/Binding/Conversion.cs
+++ Lex/CodeAnalysis/Binding/Conversion.cs
@@ -1,7 +1,35 @@
+using Lex.CodeAnalysis.Symbols;
+
 namespace Lex.CodeAnalysis.Binding
 {
     internal sealed class Conversion 
    {
-       
+       public Conversion(bool exist, bool isIdentity, bool isImplicity)
+       {
+            Exist = exist;
+            IsIdentity = isIdentity;
+            IsImplicity = isImplicity;
+        }
+
+        public bool Exist { get; }
+        public bool IsIdentity { get; }
+        public bool IsImplicity { get; }
+        public bool IsExplictic => Exist && !IsImplicity;
+
+        public static Conversion Classify(TypeSymbol from,TypeSymbol to)
+       {
+           //Identity
+           if(from == to)
+            return Conversion.Identity;
+
+            if(from == TypeSymbol.Bool || from == TypeSymbol.Int)
+            {
+                if(to == TypeSymbol.String)
+                    return Conversion.Explicty;
+            }
+
+            return Conversion.None;
+        
+       }
    }
 }
