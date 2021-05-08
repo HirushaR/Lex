557989aa0308f109c9182ba0a2f124d89facf696
HirushaR <hirusharandunu11@gmail.com>
2021-05-08 17:58:18 +0530

    fix varioble bug

--- Lex/CodeAnalysis/Binding/BoundScope.cs
+++ Lex/CodeAnalysis/Binding/BoundScope.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Lex.CodeAnalysis.Symbols;
 
 namespace Lex.CodeAnalysis.Binding
 {
     internal sealed class BoundScope
-    {        
-     private Dictionary<string, VariableSymble> _variables;
-        private Dictionary<string, FunctionSymbol> _functions;
+    {
+        private Dictionary<string, Symbol> _symbols;
 
         public BoundScope(BoundScope parent)
         {
@@ -17,69 +17,44 @@ namespace Lex.CodeAnalysis.Binding
         public BoundScope Parent { get; }
 
         public bool TryDeclareVariable(VariableSymble variable)
-        {
-            if (_variables == null)
-                _variables = new Dictionary<string, VariableSymble>();
-
-            if (_variables.ContainsKey(variable.Name))
-                return false;
-
-            _variables.Add(variable.Name, variable);
-            return true;
-        }
-
-        public bool TryLookupVariable(string name, out VariableSymble variable)
-        {
-            variable = null;
-
-            if (_variables != null && _variables.TryGetValue(name, out variable))
-                return true;
-
-            if (Parent == null)
-                return false;
-
-            return Parent.TryLookupVariable(name, out variable);
-        }
+            => TryDeclareSymbol(variable);
 
         public bool TryDeclareFunction(FunctionSymbol function)
-        {
-            if (_functions == null)
-                _functions = new Dictionary<string, FunctionSymbol>();
+            => TryDeclareSymbol(function);
 
-            if (_functions.ContainsKey(function.Name))
+        private bool TryDeclareSymbol<TSymbol>(TSymbol symbol)
+            where TSymbol : Symbol
+        {
+            if (_symbols == null)
+                _symbols = new Dictionary<string, Symbol>();
+            else if (_symbols.ContainsKey(symbol.Name))
                 return false;
 
-            _functions.Add(function.Name, function);
+            _symbols.Add(symbol.Name, symbol);
             return true;
         }
 
-        public bool TryLookupFunction(string name, out FunctionSymbol function)
+        public Symbol TryLookupSymbol(string name)
         {
-            function = null;
+            if (_symbols != null && _symbols.TryGetValue(name, out var symbol))
+                return symbol;
 
-            if (_functions != null && _functions.TryGetValue(name, out function))
-                return true;
-
-            if (Parent == null)
-                return false;
-
-            return Parent.TryLookupFunction(name, out function);
+            return Parent?.TryLookupSymbol(name);
         }
 
         public ImmutableArray<VariableSymble> GetDeclaredVariables()
-        {
-            if (_variables == null)
-                return ImmutableArray<VariableSymble>.Empty;
-
-            return _variables.Values.ToImmutableArray();
-        }
+            => GetDeclaredSymbols<VariableSymble>();
 
         public ImmutableArray<FunctionSymbol> GetDeclaredFunctions()
-        {
-            if (_functions == null)
-                return ImmutableArray<FunctionSymbol>.Empty;
+            => GetDeclaredSymbols<FunctionSymbol>();
 
-            return _functions.Values.ToImmutableArray();
+        private ImmutableArray<TSymbol> GetDeclaredSymbols<TSymbol>()
+            where TSymbol : Symbol
+        {
+            if (_symbols == null)
+                return ImmutableArray<TSymbol>.Empty;
+
+            return _symbols.Values.OfType<TSymbol>().ToImmutableArray();
         }
     }
 }
